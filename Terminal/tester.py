87373bddccc0ff409509a377fe3353e026fa2a26
alpha-256 <changmaddubstepremixer@gmail.com>
2016-09-27 13:10:49 +0700

    added stuff

--- Terminal/tester.py
+++ Terminal/tester.py
@@ -1,16 +1,8 @@
 import math
 import sys
+import os
 import time
 from functionset import functions
 
-def typeAnim(a):
-    for char in a:
-        time.sleep(0.2)
-        sys.stdout.write(char)
-        sys.stdout.flush()
-
-a = "Hello"
-b = "Welcome to CyberpunkNet!"
-typeAnim(a)
-typeAnim(b)
-print("\n")
+f = os.open('tester.txt', 'r')
+print(f)
